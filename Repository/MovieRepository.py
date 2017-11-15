c9ad1a61dbcc42470e2dd548f23b016bf069dcb1
Uc0d3 <unixcode.root@gmail.com>
2017-11-15 16:09:00 +0200

    added delete movie

--- Repository/MovieRepository.py
+++ Repository/MovieRepository.py
@@ -3,11 +3,13 @@ class MovieRepository:
     movies = []
     last_id = 0
 
-
     def add_movie(self, movie):
         movie.set_id(self.last_id + 1)
         self.movies.append(movie)
         self.last_id += 1
+
+    def delete_movie(self, movie):
+        self.movies.remove(movie)
 
     def get_by_id(self, id):
         for movie in self.movies:
